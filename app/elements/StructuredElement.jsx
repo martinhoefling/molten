bb5883967e2cfd5afd132d57b3d92c2c0a9a81d3
Martin Hoefling <martin.hoefling@gmx.de>
2016-06-09 22:19:32 +0200

    Update theme

--- app/elements/StructuredElement.jsx
+++ app/elements/StructuredElement.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router';
-
-import RaisedButton from 'material-ui/lib/raised-button';
-import JSONTree from 'react-json-tree';
-import classnames from 'classnames';
-
-import Constants from 'Constants';
-
-import styles from './StructuredElement.less';
+import React from "react";
+import {Link} from "react-router";
+import RaisedButton from "material-ui/lib/raised-button";
+import JSONTree from "react-json-tree";
+import classnames from "classnames";
+import Constants from "Constants";
+import styles from "./StructuredElement.less";
 
 const JID_REGEX = /^"(\d{20})"$/;
+
+const theme = {
+    scheme: 'molten',
+    author: 'based on solarized from ethan schoonover (http://ethanschoonover.com/solarized)',
+    base00: '#002b36',
+    base01: '#073642',
+    base02: '#586e75',
+    base03: '#657b83',
+    base04: '#839496',
+    base05: '#93a1a1',
+    base06: '#eee8d5',
+    base07: '#ffffff',
+    base08: '#dc322f',
+    base09: '#cb4b16',
+    base0A: '#b58900',
+    base0B: '#859900',
+    base0C: '#2aa198',
+    base0D: '#268bd2',
+    base0E: '#6c71c4',
+    base0F: '#d33682'
+};
 
 const StructuredElement = React.createClass({
     propTypes: {
@@ -53,6 +71,9 @@ const StructuredElement = React.createClass({
                 nodeExpanded={ this.nodeExpanded }
                 valueRenderer={ this.valueRenderer }
                 labelRenderer={ this.labelRenderer }
+                theme={{
+                    extend: theme
+                }}
             />
         );
     },
